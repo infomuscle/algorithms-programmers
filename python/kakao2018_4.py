019cf11184433d5672f956ac0482dc98a026609a
infomuscle <infomuscle10@gmail.com>
2020-02-27 19:09:11 +0900

    git ignore add

--- python/kakao2018_4.py
+++ python/kakao2018_4.py
@@ -1,30 +1,30 @@
 def solution(food_times, k):
-    answer = -1
-    cnt = 0
-    i = 0
+    answer = 0
+    timeLeft = k
+    minValue = 100000000
+    fSet = []
+    for i in range(len(food_times)):
+        fSet.append([i,food_times[i]])
+        if fSet[i][1] < minValue:
+            minValue = fSet[i][1]
+    print(fSet)
+    print(minValue)
 
-    while True:
-        if food_times[i] != 0:
-            food_times[i] -= 1
-            i += 1
-            cnt += 1
-        else:
-            i += 1
-
-        if i == len(food_times):
-            i = 0
-
-        if cnt == k:
-            break
-
-    for x in range(len(food_times)):
-        if food_times[i] != 0:
-            answer = i+1
-            break
-        else:
-            i += 1
-            if i == len(food_times):
-                i = 0
+    # while answer == 0:
+    #     temp = []
+    #     if timeLeft > minValue*len(fSet):
+    #         for x in fSet:
+    #             x[1] -= minValue
+    #             if x[1] == 0:
+    #                 temp.append(x)
+    #         timeLeft -= minValue*len(fSet)
+    #         for x in temp:
+    #             if x[1] < minValue:
+    #                 minValue = x[1]
+    #         fSet = temp
+    #     else:
+    #         print(fSet)
+    #         answer = "test"
 
     return answer
 
