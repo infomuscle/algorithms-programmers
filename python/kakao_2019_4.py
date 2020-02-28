592cee6baffa6c03828c84e1d0d44a26912832f0
infomuscle <infomuscle10@gmail.com>
2020-02-28 22:22:10 +0900

    Kakao 2019-4 무지의 먹방 라이브(정확성 Pass / 효울성 Fail)

--- python/kakao_2019_4.py
+++ python/kakao_2019_4.py
@@ -1,19 +1,25 @@
-import queue
+import queue, random, time
 
 def solution(food_times, k):
     foodsQ = queue.Queue()
-
     for i in range(len(food_times)):
         foodsQ.put([i+1, food_times[i]])
 
-    for i in range(k):
+    remainEatCnt = k
+    orderedFood = [0] + sorted(food_times)
+    for i in range(len(orderedFood)-1):
+        minFoodCnt = orderedFood[i+1] - orderedFood[i]
+        subAmount = minFoodCnt * foodsQ.qsize()
+        if remainEatCnt - subAmount > 0:
+            remainEatCnt -= subAmount
+            for j in range(foodsQ.qsize()):
+                foodsQ = queueSubtractor(foodsQ, minFoodCnt)
+        else:
+            break
+
+    for i in range(remainEatCnt):
         if foodsQ.qsize():
-            food = foodsQ.get()
-            food[1] -= 1
-            if food[1] == 0:
-                continue
-            else:
-                foodsQ.put(food)
+            foodsQ = queueSubtractor(foodsQ, 1)
         else:
             break
 
@@ -22,21 +28,33 @@ def solution(food_times, k):
     else:
         return foodsQ.get()[0]
 
-# totalFoods = 0
-# k = random.randrange(1000000, 1500000)
-# food_times = []
-# for i in range(2000):
-#     foodCnt = random.randrange(500, 1000)
-#     food_times.append(foodCnt)
-#     totalFoods += foodCnt
-#
-# print(solution(food_times, k))
-# print("k = ", k)
-# print("totalFoods = ", totalFoods)
-# print("length of food_times = ", len(food_times))
-# print("food_times = ", food_times)
+def queueSubtractor(queue,  amount):
+    food = queue.get()
+    food[1] -= amount
+    if food[1] == 0:
+        pass
+    else:
+        queue.put(food)
+    return queue
 
-# print(solution([3,1,2], 5))
-# print(solution([3,1,2], 6))
-# print(solution([3,1,2], 7))
-# print(solution([3,1,2,2], 5))
+
+totalFoods = 0
+k = random.randrange(1000000, 1500000)
+food_times = []
+for i in range(2000):
+    foodCnt = random.randrange(500, 1000)
+    food_times.append(foodCnt)
+    totalFoods += foodCnt
+
+start = time.time()
+print(solution(food_times, k))
+print("time = ", time.time() - start)
+print("k = ", k)
+print("totalFoods = ", totalFoods)
+print("length of food_times = ", len(food_times))
+print("food_times = ", food_times)
+
+print(solution([3,1,2], 5))
+print(solution([3,1,2], 6))
+print(solution([3,1,2], 7))
+print(solution([3,1,2,2], 5))
