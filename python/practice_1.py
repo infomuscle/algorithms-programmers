24c99d495fe440e63d2cb4ed33afedc0be1a3013
infomuscle <infomuscle10@gmail.com>
2020-04-07 00:07:14 +0900

    연습문제 1 - 수박수박수박수박수박수?

---
+++ python/practice_1.py
@@ -0,0 +1,20 @@
+def solution(n):
+    answer = ''
+
+    answer = "수박" * (n//2)
+
+    if n%2 == 1:
+        answer += "수"
+
+    return answer
+
+
+print(solution(1))
+print(solution(2))
+print(solution(3))
+print(solution(4))
+print(solution(5))
+print(solution(6))
+print(solution(7))
+print(solution(8))
+print(solution(10000))
