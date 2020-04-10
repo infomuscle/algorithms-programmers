7d4fb3abbcc93736c7cea5b5fd400513e9436973
infomuscle <infomuscle10@gmail.com>
2020-04-10 23:58:13 +0900

    연습문제 4 - 두 정수 사이의 합

--- python/practice_4.py
+++ python/practice_4.py
@@ -6,16 +6,17 @@ def solution(a, b):
     else:
         s, e = a, b
 
-    for i in range(s, e+1):
-        answer += i
+    for i in range((e-s+1)//2):
+        answer += (s + e)
+
+    if (e-s+1) % 2 == 1:
+        answer += (s + e) // 2
 
     return answer
-
 
 a1, b1 = 3,5
 a2, b2 = 3,3
 a3, b3 = 5,3
-
 
 print(solution(a1, b1))
 print(solution(a2, b2))
