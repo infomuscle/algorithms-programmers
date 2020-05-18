918f53bb4f02058334bdae1ec8cf9357b7f60f0b
infomuscle <infomuscle10@gmail.com>
2020-05-18 23:35:20 +0900

    연습문제 9 - 가운데 글자 가져오기

---
+++ python/practice_9.py
@@ -0,0 +1,17 @@
+def solution(s):
+    center = len(s) // 2
+
+    if len(s)%2==0:
+        answer = s[center-1] + s[center]
+    else:
+        answer = s[center]
+
+    return answer
+
+
+s1 = "abcde"
+s2 = "qwer"
+
+print(solution(s1))
+print(solution(s2))
+
