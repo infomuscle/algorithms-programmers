fc832c89e0b31ec4810e8fe27a85a1b21f4cfc6d
infomuscle <infomuscle10@gmail.com>
2020-07-12 16:00:25 +0900

    연습문제 15 - 정수 내림차순으로 배치하기

---
+++ python/practice_15.py
@@ -0,0 +1,18 @@
+def solution(n):
+    answer = 0
+    answerStr = ""
+    l = []
+    for x in str(n):
+        l.append(x)
+
+    l.sort(reverse=True)
+    for x in l:
+        answerStr += x
+
+    answer = int(answerStr)
+
+    return answer
+
+
+
+print(solution(118372))
