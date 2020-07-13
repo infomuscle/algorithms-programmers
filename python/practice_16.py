995fc85eaaa9d74c6207d8a72e10c560d977d1b9
infomuscle <infomuscle10@gmail.com>
2020-07-13 23:36:17 +0900

    연습문제 16 - 핸드폰 번호 가리기

---
+++ python/practice_16.py
@@ -0,0 +1,13 @@
+def solution(phone_number):
+    answer = ''
+
+    for i in range(len(phone_number)-4):
+        answer += "*"
+
+    answer += phone_number[len(phone_number)-4:]
+
+    return answer
+
+
+print(solution("01033334444"))
+print(solution("027778888"))
