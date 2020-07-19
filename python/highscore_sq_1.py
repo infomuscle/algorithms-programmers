7f7fc348dee1815efd240c31b7d61bed05a8f7f8
infomuscle <infomuscle10@gmail.com>
2020-07-19 12:56:51 +0900

    고득점 Kit 스택/큐 - 다리를 건너는 시간(통과 시간 체크 구현 필요)

--- python/highscore_sq_1.py
+++ python/highscore_sq_1.py
@@ -1,24 +1,49 @@
+from queue import Queue
+
 def solution(bridge_length, weight, truck_weights):
     answer = 0
 
-    truck_cnt = len(truck_weights)
+    waiting_trucks = Queue()
+    for tw in truck_weights:
+        waiting_trucks.put(tw)
 
-    pl, pr = 0, 1
-    cnt = 0
-    while pl < truck_cnt:
-        cnt += 1
-        while sum(truck_weights[pl:pr]) <= weight and pr < len(truck_weights):
-            pr += 1
-            if sum(truck_weights[pl:pr]) > weight:
-                pr -= 1
-                break
-        answer += bridge_length + pr - pl
-        pl = pr
-        pr = pl + 1
+    bridge = Queue()
+    last_on_time = 0
+    weight_on_bridge = 0
 
-    answer -= cnt - 1
+    passed_truck = Queue()
 
+    idx = 0
+    while True:
+        print(answer, print_queue(bridge), weight_on_bridge, last_on_time)
+
+        if (answer % bridge_length == 0 and answer != 0) or (bridge.qsize() != 0 and last_on_time % bridge_length == 0):
+            off_truck = bridge.get()
+            passed_truck.put(off_truck)
+            weight_on_bridge -= off_truck
+
+        if waiting_trucks.qsize() != 0 and weight_on_bridge + truck_weights[idx] <= weight:
+            on_truck = waiting_trucks.get()
+            bridge.put(on_truck)
+            weight_on_bridge += on_truck
+            last_on_time = 0
+            idx += 1
+
+        if passed_truck.qsize() == len(truck_weights) and bridge.qsize() == 0:
+            break
+
+        answer += 1
+        last_on_time += 1
     return answer
+
+def print_queue(queue):
+    qList = []
+    for i in range(queue.qsize()):
+        qList.append(queue.get())
+    for q in qList:
+        queue.put(q)
+    return qList
+
 
 
 bl1 = 2
@@ -38,6 +63,6 @@ w4 = 10
 tw4 = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 
 print(solution(bl1, w1, tw1))
-print(solution(bl2, w2, tw2))
-print(solution(bl3, w3, tw3))
-print(solution(bl4, w4, tw4))
+# print(solution(bl2, w2, tw2))
+# print(solution(bl3, w3, tw3))
+# print(solution(bl4, w4, tw4))
