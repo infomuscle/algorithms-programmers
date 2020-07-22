68c91a70e7048675bd326a4b246d37f4dc4e07bf
infomuscle <infomuscle10@gmail.com>
2020-07-23 00:38:58 +0900

    고득점 Kit 정렬 - 가장 큰 수(TC 1~6 시간 초과)

--- python/highscore_sort_2.py
+++ python/highscore_sort_2.py
@@ -1,34 +1,52 @@
 def solution(numbers):
     answer = ''
-    numbers = list(map(str, numbers))
 
-    max = ''.join(numbers)
+    numbers = list(map(str, numbers))
+    max = int(''.join(numbers))
+
+    if len(numbers) == 1:
+        return max
 
     # 하나씩 순서 바꿔보기 + 재귀
-    for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):
+    i = 0
+    start = 0
+    while True:
+        j = i + 1
+        while True:
+            # print(i, j, numbers)
             temp = swap(numbers, i, j)
-            # print(temp, numbers)
-            if int(''.join(temp)) > int(max):
+            t = int(''.join(temp))
+            if t > max:
                 numbers = temp
-                max = ''.join(numbers)
+                max = t
+                i = 0
+                j = i + 1
+            else:
+                j += 1
+                if j == len(numbers):
+                    break
+        i += 1
+        if i == len(numbers) - 1:
+            break
 
-    answer = max
+    answer = str(max)
 
     return answer
 
 def swap(numbers, i, j):
     swap = []
     swap += numbers
-    temp = swap[j]
-    swap[j] = swap[i]
-    swap[i] = temp
+    swap[i], swap[j] = swap[j], swap[i]
 
     return swap
 
 
 n1 = [6, 10, 2]
 n2 = [3, 30, 34, 5, 9]
+n3 = [0,0,0,1,0,3,0]
+n4 = [1]
 
 print(solution(n1))
 print(solution(n2))
+print(solution(n3))
+print(solution(n4))
