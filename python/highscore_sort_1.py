42fc2a8b17dd9c2d7f9245b96c971da087b6badd
infomuscle <infomuscle10@gmail.com>
2020-07-23 21:40:30 +0900

    고득점 Kit 정렬 - K번째수

--- python/highscore_sort_1.py
+++ python/highscore_sort_1.py
@@ -2,11 +2,11 @@ def solution(array, commands):
     answer = []
 
     for c in commands:
-        croped = array[c[0]-1:c[1]]
-        croped = sorted(croped)
-        answer.append(croped[c[2]-1])
+        croped = sorted(array[c[0] - 1:c[1]])
+        answer.append(croped[c[2] - 1])
 
     return answer
+
 
 a1 = [1, 5, 2, 6, 3, 7, 4]
 c1 = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
