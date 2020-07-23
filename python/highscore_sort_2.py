f5a3683663020a9eb11016308a6fa0e2459731ae
infomuscle <infomuscle10@gmail.com>
2020-07-23 20:53:39 +0900

    고득점 Kit 정렬 - 가장 큰 수(퀵 정렬 1차 실패)

--- python/highscore_sort_2.py
+++ python/highscore_sort_2.py
@@ -1,49 +1,30 @@
 def solution(numbers):
     answer = ''
-
     numbers = list(map(str, numbers))
-    max = int(''.join(numbers))
+    numbers = reversed(quick_sort(numbers))
 
-    if len(numbers) == 1:
-        return max
-
-    # 하나씩 순서 바꿔보기 + 재귀
-    i = 0
-    start = 0
-    while True:
-        j = i + 1
-        while True:
-            # print(i, j, numbers)
-            temp = swap(numbers, i, j)
-            t = int(''.join(temp))
-            if t > max:
-                numbers = temp
-                max = t
-                i = 0
-                j = i + 1
-            else:
-                j += 1
-                if j == len(numbers):
-                    break
-        i += 1
-        if i == len(numbers) - 1:
-            break
-
-    answer = str(max)
-
+    answer = ''.join(numbers)
     return answer
 
-def swap(numbers, i, j):
-    swap = []
-    swap += numbers
-    swap[i], swap[j] = swap[j], swap[i]
 
-    return swap
+def quick_sort(numbers):
+    if len(numbers) < 2:
+        return numbers
+
+    pivot = numbers[0]
+    forward, backward = [], []
+    for i in range(1, len(numbers)):
+        if int(pivot + numbers[i]) < int(numbers[i] + pivot):
+            backward.append(numbers[i])
+        else:
+            forward.append(numbers[i])
+
+    return forward + [pivot] + backward
 
 
 n1 = [6, 10, 2]
 n2 = [3, 30, 34, 5, 9]
-n3 = [0,0,0,1,0,3,0]
+n3 = [0, 0, 0, 1, 0, 3, 0]
 n4 = [1]
 
 print(solution(n1))
