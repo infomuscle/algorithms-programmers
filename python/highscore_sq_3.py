8961fffb04afb4c0535f0fa85390acd032f9737c
infomuscle <infomuscle10@gmail.com>
2020-08-09 11:38:52 +0900

    고득점 Kit 스택/큐 - 다리를 지나는 트럭(순번 변경)

--- python/highscore_sq_3.py
+++ python/highscore_sq_3.py
@@ -1,36 +1,60 @@
 from collections import deque
 
 
-def solution(prices):
-    answer = []
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
 
-    stk = []
+    waiting_trucks = deque(truck_weights)
 
-    for i in range(len(prices)):
-        num = prices[i]
-        stk.append(num)
-        que = deque(prices[i + 1:])
+    bridge = deque()
+    times_on_bridge = deque()
+    weight_on_bridge = 0
 
-        # print(stk, que)
+    passed_trucks = deque()
 
-        for j in range(len(que)):
-            if que.popleft() < num:
-                stk.pop()
-                answer.append(j + 1)
-                break
-        if len(stk) != 0:
-            stk.pop()
-            answer.append(len(prices) - 1 - i)
+    idx = 0
+    while True:
+        if answer != 0:
+            time = times_on_bridge.popleft()
+            if answer - time >= bridge_length:
+                off_truck = bridge.popleft()
+                passed_trucks.appendleft(off_truck)
+                weight_on_bridge -= off_truck
+            else:
+                times_on_bridge.appendleft(time)
+
+        if len(waiting_trucks) != 0 and weight_on_bridge + truck_weights[idx] <= weight:
+            on_truck = waiting_trucks.popleft()
+            bridge.append(on_truck)
+            times_on_bridge.append(answer)
+            weight_on_bridge += on_truck
+            idx += 1
+
+        answer += 1
+
+        if len(bridge) == 0:
+            break
 
     return answer
 
 
-p1 = [1, 2, 3, 2, 3]
-p2 = [5, 4, 3, 3, 2]
-p3 = [1, 2, 3, 4, 5]
-p4 = [3, 3, 3, 2, 3]
+bl1 = 2
+w1 = 10
+tw1 = [7, 4, 5, 6]
 
-print(solution(p1))
-print(solution(p2))
-print(solution(p3))
-print(solution(p4))
+bl2 = 100
+w2 = 100
+tw2 = [10]
+
+bl3 = 100
+w3 = 100
+tw3 = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+
+bl4 = 1
+w4 = 10
+tw4 = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+
+print(solution(bl1, w1, tw1))
+print(solution(bl2, w2, tw2))
+print(solution(bl3, w3, tw3))
+print(solution(bl4, w4, tw4))
