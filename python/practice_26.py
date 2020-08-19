a87fcbcd4d2584ce27499711b4d7e966230af015
infomuscle <infomuscle10@gmail.com>
2020-08-19 19:41:49 +0900

    연습문제 26 - 콜라츠 추측

---
+++ python/practice_26.py
@@ -0,0 +1,26 @@
+def solution(num):
+    answer = 0
+
+    while num != 1:
+        if answer == 500:
+            return -1
+
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num = num * 3 + 1
+
+        answer += 1
+
+    return answer
+
+
+
+n1 = 6
+n2 = 16
+n3 = 626331
+
+
+print(solution(n1))
+print(solution(n2))
+print(solution(n3))
