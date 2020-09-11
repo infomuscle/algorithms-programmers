edec052bed8c55f3314e70083f86e1257ec8242b
infomuscle <infomuscle10@gmail.com>
2020-09-11 18:38:19 +0900

    고득점 Kit DFS/BFS - 타겟 넘버(완전탐색 시간초과)

--- python/highscore_dfsbfs_1.py
+++ python/highscore_dfsbfs_1.py
@@ -1,65 +1,31 @@
-class Node:
-    def __init__(self, index, value):
-        self.index = index
-        self.value = value
-        self.children = [None, None]
-
-    def left_child(self, node):
-        self.children[0] = node
-        return self
-
-    def right_child(self, node):
-        self.children[1] = node
-        return self
-
-
 def solution(numbers, target):
     answer = 0
-    tree = get_tree(numbers)
+
+    mes = [[]]
+
+    for i in range(len(numbers)):
+        tmps = []
+        for m in mes:
+            tmp1 = m + [numbers[i]]
+            tmp2 = m + [-numbers[i]]
+            if tmp1 not in mes:
+                tmps.append(tmp1)
+            if tmp2 not in mes:
+                tmps.append(tmp2)
+        mes = tmps
+
+    for m in mes:
+        if sum(m) == target:
+            answer += 1
+
+    # print(mes)
+    # print(len(mes))
 
     return answer
-
-
-def dfs(graph, start_node):
-    visited = list()
-    stack = list()
-    stack.append(start_node)
-
-    while stack:
-        node = stack.pop()
-        if node not in visited:
-            visited.append(node)
-            stack.extend(graph[node] - set(visited))
-
-    return visited
-
-
-def get_tree(numbers):
-    tree = {}
-
-    node = Node(0, numbers[0])
-    node.left_child(Node(1, numbers[1]))
-    node.right_child(Node(1, -numbers[1]))
-    tree[node] = {node.children[0], node.children[1]}
-
-    for i in range(1, len(numbers) - 1):
-        node1 = Node(i, numbers[i])
-        node1.left_child(Node(i + 1, numbers[i + 1]))
-        node1.right_child(Node(i + 1, -numbers[i + 1]))
-        tree[node1] = {node1.children[0], node1.children[1]}
-
-        node2 = Node(i, -numbers[i])
-        node2.left_child(Node(i + 1, numbers[i + 1]))
-        node2.right_child(Node(i + 1, -numbers[i + 1]))
-        tree[node2] = {node2.children[0], node2.children[1]}
-
-    for t in tree:
-        print(str(t.index) + ":" + str(t.value) + ":" + str(t.children[0].value) + ":" + str(t.children[1].value))
-
-    return tree
 
 
 n1 = [1, 1, 1, 1, 1]
 n2 = [1, 2, 3, 4, 5]
 
-print(get_tree(n2))
+print(solution(n1, 3))
+print(solution(n2, 3))
