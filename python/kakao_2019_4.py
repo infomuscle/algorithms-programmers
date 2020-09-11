a007cbd62b5faa9de53fdc41c756ccdf4fd295b2
infomuscle <infomuscle10@gmail.com>
2020-09-11 23:06:23 +0900

    Kakao 2019-4 무지의 먹방 라이브(정확성)

--- python/kakao_2019_4.py
+++ python/kakao_2019_4.py
@@ -1,77 +1,60 @@
-from queue import PriorityQueue
-from queue import Queue
-import random, time
+from collections import OrderedDict
+from collections import deque
 
 
 def solution(food_times, k):
+    answer = 0
 
-    totalFoodAmt = 0
-
-    pFoodsQ = PriorityQueue()
-    for i in range(len(food_times)):
-        pFoodsQ.put((food_times[i], i+1))
-        totalFoodAmt += food_times[i]
-
-    if k >= totalFoodAmt:
+    if sum(food_times) <= k:
         return -1
 
-    remainEatCnt = k
-    eatenAmt = 0
-    while pFoodsQ.qsize():
-        food = pFoodsQ.get()
-        # print(food)
-        subAmt = (pFoodsQ.qsize()+1) * (food[0]-eatenAmt)
-        # print(remainEatCnt)
-        if remainEatCnt - subAmt > 0:
-            remainEatCnt -= subAmt
-            eatenAmt += food[0] - eatenAmt
-        else:
-            if pFoodsQ.qsize():
-                pFoodsQ.put(food)
-            break
+    foods = OrderedDict()
+    for i, ft in enumerate(food_times):
+        foods[i] = ft
 
-    foodList = []
-    while pFoodsQ.qsize():
-        food = pFoodsQ.get()
-        foodList.append([food[1], food[0]-eatenAmt])
-    foodList = sorted(foodList)
-    # print(remainEatCnt)
+    time_passed = 0
+    while time_passed + len(foods) < k:
+        time_passed += len(foods)
 
-    nFoodsQ = Queue()
-    for i in range(len(foodList)):
-        nFoodsQ.put(foodList[i])
-        # print(foodList[i])
+        key_to_delete = []
+        for fk in foods.keys():
+            foods[fk] -= 1
+            if foods[fk] == 0:
+                key_to_delete.append(fk)
 
-    for i in range(remainEatCnt):
-        if nFoodsQ.qsize():
-            food = nFoodsQ.get()
-            food[1] -= 1
-            if food[1] > 0:
-                nFoodsQ.put(food)
-            else:
-                continue
-        else:
-            return -1
+        for ktd in key_to_delete:
+            del foods[ktd]
 
-    if nFoodsQ.qsize():
-        answer = nFoodsQ.get()[0]
-    else:
-        answer = -1
+    time_left = k - time_passed
+    queue = deque()
+    for fk in foods.keys():
+        queue.append({fk: foods[fk]})
+
+    for i in range(time_left):
+        food = queue.popleft()
+        queue.append(food)
+
+    answer = list(queue.popleft().keys())[0] + 1
 
     return answer
 
-print(solution([5,4,3,2,1], 1))
-print(solution([5,4,3,2,1], 2))
-print(solution([5,4,3,2,1], 3))
-print(solution([5,4,3,2,1], 4))
-print(solution([5,4,3,2,1], 5))
-print(solution([5,4,3,2,1], 6))
-print(solution([5,4,3,2,1], 7))
-print(solution([5,4,3,2,1], 8))
-print(solution([5,4,3,2,1], 9))
-print(solution([5,4,3,2,1], 10))
-print(solution([5,4,3,2,1], 11))
-print(solution([5,4,3,2,1], 12))
-print(solution([5,4,3,2,1], 13))
-print(solution([5,4,3,2,1], 14))
-print(solution([5,4,3,2,1], 15))
+
+print(solution([5, 4, 3, 2, 1], 1))
+print(solution([5, 4, 3, 2, 1], 2))
+print(solution([5, 4, 3, 2, 1], 3))
+print(solution([5, 4, 3, 2, 1], 4))
+print(solution([5, 4, 3, 2, 1], 5))
+print(solution([5, 4, 3, 2, 1], 6))
+print(solution([5, 4, 3, 2, 1], 7))
+print(solution([5, 4, 3, 2, 1], 8))
+print(solution([5, 4, 3, 2, 1], 9))
+print(solution([5, 4, 3, 2, 1], 10))
+print(solution([5, 4, 3, 2, 1], 11))
+print(solution([5, 4, 3, 2, 1], 12))
+print(solution([5, 4, 3, 2, 1], 13))
+print(solution([5, 4, 3, 2, 1], 14))
+print(solution([5, 4, 3, 2, 1], 15))
+
+f1 = [3, 1, 2]
+k1 = 5
+print(solution(f1, k1))
