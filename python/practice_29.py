b61792b848015e572a2ba6399813633d52437eee
infomuscle <infomuscle10@gmail.com>
2020-09-19 18:12:53 +0900

    연습문제 29 - 두 개 뽑아서 더하기

---
+++ python/practice_29.py
@@ -0,0 +1,17 @@
+def solution(numbers):
+    answer_set = set()
+    for i in range(len(numbers)):
+        for j in range(len(numbers)):
+            if i == j:
+               continue
+            answer_set.add(numbers[i] + numbers[j])
+    answer = sorted(list(answer_set))
+
+    return answer
+
+
+n1 = [2, 1, 3, 4, 1]
+n2 = [5, 0, 2, 7]
+
+print(solution(n1))
+print(solution(n2))
