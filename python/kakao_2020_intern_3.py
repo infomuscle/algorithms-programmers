05463f5f1c144e9e37767aa9d7a9e511e6a6ca5d
infomuscle <infomuscle10@gmail.com>
2020-09-22 02:54:17 +0900

    Kakao 2020 인턴 3 - 보석 쇼핑(이진탐색 정확성 통과)

--- python/kakao_2020_intern_3.py
+++ python/kakao_2020_intern_3.py
@@ -8,17 +8,11 @@ def solution(gems):
     pl, pr = 0, current_length - 1
 
     possibles = []
-    step = 0
     while True:
-        step += 1
-        print(step)
-
-        print(min_length, max_length, current_length)
         gems_tmp = set(gems[pl:pr + 1])
-        print(gems_tmp)
         if gems_set == gems_tmp:
-            possibles.append((pl+1, pr+1))
-            max_length = pr - pl + 1
+            possibles.append((pl + 1, pr + 1))
+            max_length = pr - pl + 1 - 1
             if (min_length + max_length) // 2 == current_length:
                 break
             else:
@@ -28,15 +22,15 @@ def solution(gems):
             pl += 1
             pr += 1
             if pr == len(gems):
-                min_length = pr - pl + 1
+                min_length = pr - pl + 1 + 1
                 if (min_length + max_length) // 2 == current_length:
                     break
                 else:
                     current_length = (min_length + max_length) // 2
                 pl, pr = 0, current_length - 1
 
-    print("possibles: ", possibles)
-    answer = []
+    # print("possibles: ", possibles)
+    answer = list(possibles[-1])
     return answer
 
 
@@ -45,7 +39,7 @@ g2 = ["AA", "AB", "AC", "AA", "AC"]
 g3 = ["XYZ", "XYZ", "XYZ"]
 g4 = ["ZZZ", "YYY", "NNNN", "YYY", "BBB"]
 
-# print(solution(g1))
-# print(solution(g2))
-# print(solution(g3))
+print(solution(g1))
+print(solution(g2))
+print(solution(g3))
 print(solution(g4))
