8565d8aa10b84202c722a549d71bf690c2e4c6fc
infomuscle <infomuscle10@gmail.com>
2020-09-21 22:57:34 +0900

    Kakao 2020 인턴 2 - 수식 최대화

--- python/kakao_2020_intern_2.py
+++ python/kakao_2020_intern_2.py
@@ -6,45 +6,48 @@ def solution(expression):
 
     operators = ["+", "-", "*"]
 
+    # 순열로 우선순위 맵 생성
     priorities = dict()
     for i, p in enumerate(permutations(operators, 3)):
         priorities[i] = list(p)
-    print(priorities)
 
-    expression_split = list()
+    # 중위표기 문자열 수식을 리스트형 수식으로 변
+    infix = list()
     ptr = 0
     for i, e in enumerate(expression):
         if e in operators:
-            expression_split.append(expression[ptr:i])
-            expression_split.append(expression[i])
+            infix.append(expression[ptr:i])
+            infix.append(expression[i])
             ptr = i + 1
-    expression_split.append(expression[ptr:])
-    print(expression_split)
-    print("-----------")
+    infix.append(expression[ptr:])
 
+    # 각 우선순위별로 최대값 계산
     for priority in priorities.values():
-        postfix = []
-        stack = []
-        for e in expression_split:
+
+        # 중위표기 리스트형 수식을 후위 리스트형 수식으로 변경
+        postfix, stack = list(), list()
+        for e in infix:
             if e not in operators:
                 postfix.append(e)
             else:
-                while len(stack) != 0 and priority.index(stack[-1]) < priority.index(e):
+                while len(stack) != 0 and priority.index(stack[-1]) <= priority.index(e):
                     postfix.append(stack.pop())
                 stack.append(e)
-        postfix.extend(stack)
-        print(priority, postfix)
+        while len(stack) != 0:
+            postfix.append(stack.pop())
 
-        stk = []
-        for p in postfix:
-            if p not in operators:
-                stk.append(p)
+        # 후위표기 리스트에 대한 계산 수
+        stk = list()
+        for e in postfix:
+            if e not in operators:
+                stk.append(e)
             else:
-                stk.append(calculator(p, int(stk.pop()), int(stk.pop())))
+                stk.append(calculator(e, int(stk.pop()), int(stk.pop())))
 
-        reward = stk.pop()
-        if abs(reward) > answer:
-            answer = abs(reward)
+        # 상금 절대값 비교 후 최대값 세
+        reward = abs(stk.pop())
+        if reward > answer:
+            answer = reward
 
     return answer
 
@@ -53,7 +56,7 @@ def calculator(op: str, num1: int, num2: int):
     if op == "+":
         result = num1 + num2
     elif op == "-":
-        result = num1 - num2
+        result = num2 - num1
     else:
         result = num1 * num2
 
@@ -64,5 +67,5 @@ e1 = "100-200*300-500+20"
 e2 = "50*6-3*2"
 
 print(solution(e1))
-print("###########")
+# print("###########")
 print(solution(e2))
