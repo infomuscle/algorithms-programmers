d8a2bba5cbd33c6216e04255d609f05c8c72939c
infomuscle <infomuscle10@gmail.com>
2020-09-22 02:37:48 +0900

    Kakao 2020 인턴 3 - 보석 쇼핑(이진탐색 응용 백업)

--- python/kakao_2020_intern_3.py
+++ python/kakao_2020_intern_3.py
@@ -1,20 +1,42 @@
 def solution(gems):
     gems_set = set(gems)
 
-    pl, pr = 0, len(gems_set) - 1
+    min_length = len(gems_set)
+    max_length = len(gems)
 
+    current_length = (min_length + max_length) // 2
+    pl, pr = 0, current_length - 1
+
+    possibles = []
     step = 0
     while True:
-        if gems_set == set(gems[pl:pr + 1]):
-            break
+        step += 1
+        print(step)
+
+        print(min_length, max_length, current_length)
+        gems_tmp = set(gems[pl:pr + 1])
+        print(gems_tmp)
+        if gems_set == gems_tmp:
+            possibles.append((pl+1, pr+1))
+            max_length = pr - pl + 1
+            if (min_length + max_length) // 2 == current_length:
+                break
+            else:
+                current_length = (min_length + max_length) // 2
+            pl, pr = 0, current_length - 1
         else:
             pl += 1
             pr += 1
             if pr == len(gems):
-                step += 1
-                pl, pr = 0, len(gems_set) - 1 + step
+                min_length = pr - pl + 1
+                if (min_length + max_length) // 2 == current_length:
+                    break
+                else:
+                    current_length = (min_length + max_length) // 2
+                pl, pr = 0, current_length - 1
 
-    answer = [pl + 1, pr + 1]
+    print("possibles: ", possibles)
+    answer = []
     return answer
 
 
@@ -23,7 +45,7 @@ g2 = ["AA", "AB", "AC", "AA", "AC"]
 g3 = ["XYZ", "XYZ", "XYZ"]
 g4 = ["ZZZ", "YYY", "NNNN", "YYY", "BBB"]
 
-print(solution(g1))
-print(solution(g2))
-print(solution(g3))
+# print(solution(g1))
+# print(solution(g2))
+# print(solution(g3))
 print(solution(g4))
