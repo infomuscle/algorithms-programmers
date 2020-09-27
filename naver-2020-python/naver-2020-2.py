0441dbc8cf3d8cca092ab27e9ba5b8764a07c044
infomuscle <infomuscle10@gmail.com>
2020-09-27 11:36:11 +0900

    Naver 2020

--- naver-2020-python/naver-2020-2.py
+++ naver-2020-python/naver-2020-2.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def solution(blocks):
     answer = []
 
@@ -6,20 +9,25 @@ def solution(blocks):
         if i not in block_map.keys():
             block_map[i] = dict()
         block_map[i][block[0]] = block[1]
-    print(block_map)
 
-    for row in block_map.keys():
-        print(row)
-        for col in block_map[row].keys():
-            try:
-                left = block_map[row + 1][col]
-            except:
-                block_map[row + 1][col] = block_map[row][col] - block_map[row + 1][col + 1]
-            try:
-                right = block_map[row + 1][col + 1]
-            except:
-                block_map[row + 1][col + 1] = block_map[row][col] - block_map[row + 1][col]
-        print(block_map)
+    last_row = sorted(block_map.keys())[-1]
+    answer.append(block_map[0][0])
+    for i, row in enumerate(block_map.keys()):
+        if i == last_row:
+            break
+
+        block_on_row = deque(block_map[row].items())
+        while len(block_map[row]) < row + 1 or len(block_map[row + 1].keys()) < row + 1 + 1:
+            block = block_on_row.popleft()
+            if block[0] not in block_map[row + 1].keys() and block[0] + 1 in block_map[row + 1].keys():
+                block_map[row + 1][block[0]] = block_map[row][block[0]] - block_map[row + 1][block[0] + 1]
+            elif block[0] in block_map[row + 1].keys() and block[0] + 1 not in block_map[row + 1].keys():
+                block_map[row + 1][block[0] + 1] = block_map[row][block[0]] - block_map[row + 1][block[0]]
+            else:
+                block_on_row.append(block)
+
+        for b in sorted(block_map[row + 1].keys()):
+            answer.append(block_map[row + 1][b])
 
     return answer
 
