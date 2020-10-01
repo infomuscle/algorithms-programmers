b2df95d913e50c64b1ba42c4faec5c961008ccee
infomuscle <infomuscle10@gmail.com>
2020-10-01 12:00:24 +0900

    연습문제 31 - 소수찾기(시간초과)

---
+++ python/practice_31.py
@@ -0,0 +1,26 @@
+def solution(n):
+    answer = 0
+
+    primes = [2]
+
+    for i in range(2, n + 1):
+        is_prime = True
+        for p in primes:
+            if i % p == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(i)
+
+    answer = len(primes)
+
+    return answer
+
+
+n1 = 10
+n2 = 5
+n3 = 1000000
+
+print(solution(n1))
+print(solution(n2))
+print(solution(n3))
