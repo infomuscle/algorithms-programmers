6823b1b648106f591313a53b3aac5f78983f565b
infomuscle <infomuscle10@gmail.com>
2020-10-04 01:45:41 +0900

    연습문제 37 - N개의 최소공배수

---
+++ python/practice_37.py
@@ -0,0 +1,29 @@
+def solution(arr):
+    arr = sorted(arr)
+
+    num = arr[-1]
+    adder = arr[-1]
+
+    while True:
+        is_lcm = True
+        for a in arr:
+            if num % a != 0:
+                is_lcm = False
+                break
+        if is_lcm:
+            break
+        else:
+            num += adder
+
+    return num
+
+
+a1 = [2, 6, 8, 14]
+a2 = [1, 2, 3]
+a3 = [1, 2, 3, 7, 17, 22, 23, 100]
+a4 = [1, 2, 3, 4]
+
+print(solution(a1))
+print(solution(a2))
+print(solution(a3))
+print(solution(a4))
