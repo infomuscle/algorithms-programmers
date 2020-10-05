644990614c982bb0c5642856fcbd3890af5705c2
infomuscle <infomuscle10@gmail.com>
2020-10-05 17:01:14 +0900

    연습문제 38 - 124 나라의 숫자(백업)

--- python/practice_38.py
+++ python/practice_38.py
@@ -1,65 +1,58 @@
 def solution(n):
-    answer = ''
+    answer = ""
 
-    map = {"1": "1", "2": "2", "3": "4", }
+    map1 = {1: "A", 2: "B", 0: "C"}
+    map2 = {"A": 1, "B": 2, "C": 3}
+    map3 = {1: "A", 2: "B", 3: "C"}
 
-    bound = 0
+    chrs = ""
+    remains = n % 3
+    chr = str(map1[remains])
+    chrs += chr
+    n -= map2[chr]
+
     square = 1
     while True:
-        bound += 3 ** square
-        if bound >= n:
+        if n == 0:
             break
+        for i in range(3, 0, -1):
+            if n == ((3 ** square) * i):
+                chrs += map3[i]
+                n -= (3 ** square) * i
+                break
         square += 1
-
-    tmp = ""
-    while True:
-        square -= 1
-        a = str(n // 3 ** square)
-        if a != "0":
-            tmp += a
-        else:
-            tmp = tmp[:len(tmp) - 1] + str(int(tmp[-1]) - 1) + "3"
-        n = n % 3 * square
-        if square == 0:
-            break
-
-    for t in tmp:
-        answer += map[t]
-
+    answer = chrs[::-1]
     return answer
 
 
-n1 = 1
-n2 = 2
-n3 = 3
-n4 = 4
-n5 = 5
-n6 = 6
-n7 = 7
-n8 = 8
+n1 = 1  # A 3^0*A
+n2 = 2  # B 3^0*B
+n3 = 3  # C 3*0*C
 
-print(solution(n1))
-print(solution(n2))
-print(solution(n3))
-print(solution(n4))
-print(solution(n5))
-print(solution(n6))
-print(solution(n7))
-print(solution(n8))
+n4 = 4  # AA 3^1*A + 3^0*A
+n5 = 5  # AB 3^1*A + 3^0*B
+n6 = 6  # AC 3^1*A + 3^0*C
 
-# 1 = A 3^0 * 1
-# 2 = B 3^0 * 2
-# 3 = C 3^0 * 3
-#
-# 4 = AA 3^1 * 1 + 3^0*1
-# 5 = AB
-# 6 = AC
-# 7 = BA
-# 8 = BB
-# 9 = BC
-# 10 = CA
-# 11 = CB
-# 12 = CC 3^1 * 3 + 3^0 * 3
+n7 = 7  # BA 3^1*B + 3^0*A
+n8 = 8  # BB 3^1*B + 3^0*B
+n9 = 9  # BC 3^1*B + 3^0*C
 
-#
-# 13 = AAA
+n10 = 10  # CA 3^1*C + 3^0*A
+n11 = 11  # CB 3^1*C + 3^0*B
+n12 = 12  # CC 3^1*C + 3^0*C
+
+n13 = 13  # AAA 3^2*A + 3^1*A + 3^0*A
+
+# print(solution(n1))
+# print(solution(n2))
+# print(solution(n3))
+# print(solution(n4))
+# print(solution(n5))
+# print(solution(n6))
+# print(solution(n7))
+# print(solution(n8))
+# print(solution(n9))
+# print(solution(n10))
+# print(solution(n11))
+# print(solution(n12))
+print(solution(n13))
