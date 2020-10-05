cf38fd5ab3e3628800829ba2a98f2d09d4785ee3
infomuscle <infomuscle10@gmail.com>
2020-10-05 14:30:38 +0900

    연습문제 39 - 피보나치 수

--- python/practice_39.py
+++ python/practice_39.py
@@ -1,26 +1,30 @@
 def solution(n):
-    # answer = fibonacci(n)
     answer = fibonacci(n) % 1234567
     return answer
 
 
 def fibonacci(n):
+    fibonaccis = [0, 1]
     if n == 0:
         return 0
-    if n == 1:
+    elif n == 1:
         return 1
+    else:
+        for i in range(n - 1):
+            fibonaccis.append(fibonaccis[-1] + fibonaccis[-2])
 
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    return fibonaccis[-1]
 
 
-# print(solution(0))
-# print(solution(1))
-# print(solution(2))
-# print(solution(3))
-# print(solution(4))
-# print(solution(5))
-# print(solution(6))
-# print(solution(7))
-# print(solution(8))
-# print(solution(9))
+print(solution(0))
+print(solution(1))
+print(solution(2))
+print(solution(3))
+print(solution(4))
+print(solution(5))
+print(solution(6))
+print(solution(7))
+print(solution(8))
+print(solution(9))
 print(solution(100))
+print(solution(100000))
