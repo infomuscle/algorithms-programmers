e41cf7c6f08872d03331fb3f382c1bdb72468dab
infomuscle <infomuscle10@gmail.com>
2020-10-08 13:41:54 +0900

    연습문제 40 - 최솟값 만들기

---
+++ python/practice_40.py
@@ -0,0 +1,18 @@
+def solution(A, B):
+    answer = 0
+
+    A, B = sorted(A), sorted(B)
+
+    for i in range(len(A)):
+        answer += A[i] * B[len(B) - 1 - i]
+
+    return answer
+
+
+A1 = [1, 4, 2]
+B1 = [5, 4, 4]
+A2 = [1, 2]
+B2 = [3, 4]
+
+print(solution(A1, B1))
+print(solution(A2, B2))
