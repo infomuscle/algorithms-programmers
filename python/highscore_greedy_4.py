ae9758d010be9f03ae63f8d7485206e3de2c2d8d
infomuscle <infomuscle10@gmail.com>
2020-10-08 16:17:49 +0900

    고득점 Kit 탐욕법 - 구명보트(정확성 12/15)

--- python/highscore_greedy_4.py
+++ python/highscore_greedy_4.py
@@ -2,31 +2,23 @@ from collections import deque
 
 
 def solution(people, limit):
-    on_boat_sum = 0
-    people_on_boat = list()
-    boats = 0
+    answer = 0
 
-    people = sorted(people, reverse=True)
-    left_queue = deque(enumerate(people))
-
-    while on_boat_sum != len(people):
-        on_boat = 0
-        weight_of_boat = 0
-        unboated_queue = deque()
-        for i in range(len(left_queue)):
-            p = left_queue.popleft()
-            if p[0] not in people_on_boat and weight_of_boat + p[1] <= limit:
-                on_boat += 1
-                people_on_boat.append(p[0])
-                weight_of_boat += p[1]
+    lefts = deque(enumerate(sorted(people, reverse=True)))
+    while len(lefts) != 0:
+        unboated = deque()
+        weight_on_boat = 0
+        for i in range(len(lefts)):
+            p = lefts.popleft()
+            if weight_on_boat + p[1] <= limit:
+                weight_on_boat += p[1]
             else:
-                unboated_queue.append(p)
+                unboated.append(p)
 
-        left_queue = unboated_queue
-        boats += 1
-        on_boat_sum += on_boat
+        lefts = unboated
+        answer += 1
 
-    return boats
+    return answer
 
 
 p1 = [70, 50, 80, 50]
