7335ec1e7af0cd9e2512aa7726924d7c7528f8e5
infomuscle <infomuscle10@gmail.com>
2020-10-11 18:52:20 +0900

    코드 챌린지 시즌1 - 두 개 뽑아서 더하기

--- python/code_challenge_s1_1.py
+++ python/code_challenge_s1_1.py
@@ -3,7 +3,7 @@ def solution(numbers):
     for i in range(len(numbers)):
         for j in range(len(numbers)):
             if i == j:
-               continue
+                continue
             answer_set.add(numbers[i] + numbers[j])
     answer = sorted(list(answer_set))
 
