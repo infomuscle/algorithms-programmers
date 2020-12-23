637d65ab5fde27ea4eb356efb34e547e8c1b5161
infomuscle <infomuscle10@gmail.com>
2020-12-23 17:23:39 +0900

    고득점 Kit DFS/BFS - 여행경로(중복 티켓)

--- python/highscore_dfsbfs_4.py
+++ python/highscore_dfsbfs_4.py
@@ -2,21 +2,24 @@ def solution(tickets):
     answer = []
 
     ticket_map = dict()
-    for ticket in tickets:
+    for i, ticket in enumerate(tickets):
         if ticket[0] not in ticket_map:
             ticket_map[ticket[0]] = set()
+        ticket.append(i)
         ticket_map[ticket[0]].add(tuple(ticket))
+    # print(ticket_map)
 
     used_tickets = []
     stack = sorted(list(ticket_map["ICN"]), reverse=True)
     while stack:
-        node = stack.pop()
-        if node not in used_tickets:
-            if node[1] in ticket_map:
-                used_tickets.append(node)
-                stack.extend(sorted(list(ticket_map[node[1]] - set(used_tickets)), reverse=True))
+        ticket = stack.pop()
+        if ticket not in used_tickets:
+            arrival = ticket[1]
+            if arrival in ticket_map:
+                used_tickets.append(ticket)
+                stack.extend(sorted(list(ticket_map[arrival] - set(used_tickets)), reverse=True))
             elif len(used_tickets) == len(tickets) - 1:
-                used_tickets.append(node)
+                used_tickets.append(ticket)
                 break
 
     answer.append("ICN")
@@ -26,12 +29,18 @@ def solution(tickets):
     return answer
 
 
-t1 = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
-t2 = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]
+t1 = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]  # ICN - JFK - HND - IAD
+t2 = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]  # ICN - ATL - ICN - SFO - ATL -SFO
 t3 = [["ICN", "SFO"], ["SFO", "ATL"], ["SFO", "JFK"], ["JFK", "SFO"]]  # ICN - SFO - JFK - SFO - ATL
 t4 = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "JFK"], ["JFK", "ICN"]]  # ICN - SFO - JFK - ICN - ATL
+t5 = [["ICN", "HND"], ["ICN", "SFO"], ["ICN", "JFK"], ["JFK", "ICN"], ["HND", "ICN"]]  # ICN - HND - ICN - JFK - ICN - SFO
+t6 = [["ICN", "JFK"], ["JFK", "ATL"], ["JFK", "IAD"], ["IAD", "HND"], ["HND", "JFK"]]  # ICN - JFK - IAD - HND - JFK - ATL
+t7 = [["ICN", "JFK"], ["JFK", "ICN"], ["ICN", "JFK"], ["JFK", "ICN"]]  # ICN - JFK - ICN - JFK - ICN
 
-# print(solution(t1))
-# print(solution(t2))
-# print(solution(t3))
+print(solution(t1))
+print(solution(t2))
+print(solution(t3))
 print(solution(t4))
+print(solution(t5))
+print(solution(t6))
+print(solution(t7))
