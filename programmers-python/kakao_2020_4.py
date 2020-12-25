876605ecf7eaabdfa449e0724551dc3057bcbde5
infomuscle <infomuscle10@gmail.com>
2020-12-26 01:33:25 +0900

    Kakao 2020-4 가사 검색

--- programmers-python/kakao_2020_4.py
+++ programmers-python/kakao_2020_4.py
@@ -1,59 +1,57 @@
 def solution(words, queries):
     answer = []
 
-    word_map = {}
+    trie = {}
+    trie_rev = {}
     for word in words:
         word_length = len(word)
-        if word_length not in word_map:
-            word_map[word_length] = {}
-            word_map[word_length]["first"] = {}
-            word_map[word_length]["last"] = {}
-        if word[0] not in word_map[word_length]["first"]:
-            word_map[word_length]["first"][word[0]] = []
-        word_map[word_length]["first"][word[0]].append(word)
-        if word[-1] not in word_map[word_length]["last"]:
-            word_map[word_length]["last"][word[-1]] = []
-        word_map[word_length]["last"][word[-1]].append(word)
 
-    found = {}
+        if word_length not in trie:
+            trie[word_length] = [0, {}]
+        trie[word_length][0] += 1
+
+        node = trie[word_length][1]
+        for w in word:
+            if w not in node:
+                node[w] = [0, {}]
+            node[w][0] += 1
+            node = node[w][1]
+
+        word = word[::-1]
+
+        if word_length not in trie_rev:
+            trie_rev[word_length] = [0, {}]
+        trie_rev[word_length][0] += 1
+
+        node = trie_rev[word_length][1]
+        for w in word:
+            if w not in node:
+                node[w] = [0, {}]
+            node[w][0] += 1
+            node = node[w][1]
+
     for query in queries:
-        if query in found:
-            answer.append(found[query])
+        query_length = len(query)
+        if query_length not in trie:
+            answer.append(0)
             continue
 
-        cnt = 0
-        full_length = len(query)
-
-        comp_in_length = word_map.get(full_length, None)
-        if comp_in_length == None:
-            answer.append(cnt)
-            continue
-
-        q = query.replace("?", "")
-        if q == "":
-            for key in comp_in_length["first"]:
-                cnt += len(comp_in_length["first"][key])
-            answer.append(cnt)
-            continue
-
-        if query[-1] == "?":
-            comparatives = comp_in_length["first"].get(query[0], None)
+        if query[0] != "?":
+            node = trie[query_length]
         else:
-            comparatives = comp_in_length["last"].get(query[-1], None)
-        if comparatives == None:
-            answer.append(cnt)
-            continue
+            node = trie_rev[query_length]
+            query = query[::-1]
 
-        for comparative in comparatives:
-            if query[-1] == "?":
-                match = comparative[:len(q)]
+        for q in query:
+            if q != "?":
+                node = node[1].get(q, None)
+                if node == None:
+                    answer.append(0)
+                    break
+                continue
             else:
-                match = comparative[full_length - len(q):]
-            if q == match:
-                cnt += 1
-
-        answer.append(cnt)
-        found[query] = cnt
+                answer.append(node[0])
+                break
 
     return answer
 
