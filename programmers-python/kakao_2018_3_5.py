c54507aaaea19524a3c8232ff3b8e57710423e62
infomuscle <infomuscle10@gmail.com>
2020-12-30 15:24:33 +0900

    Kakao 2018-3-5 [3차] 방금그곡

--- programmers-python/kakao_2018_3_5.py
+++ programmers-python/kakao_2018_3_5.py
@@ -1,36 +1,26 @@
+from datetime import datetime
+
+
 def solution(m, musicinfos):
     melody = convert_chords(m)
 
     musics = []
     for i, musicinfo_str in enumerate(musicinfos):
         music_info = musicinfo_str.split(",")
-
         play_time = get_play_time(music_info[0], music_info[1])
-        if len(melody) > play_time:
-            continue
-
         sheet = convert_chords(music_info[3])
         played_chords = (sheet * (play_time // len(sheet) + 1))[:play_time]
-
-        for j in range(len(played_chords) - len(melody) + 1):
-            if played_chords[j:j + len(melody)] == melody:
-                musics.append((music_info[2], play_time, i))
-                break
+        if " ".join(melody) + " " in " ".join(played_chords) + " ":
+            musics.append((music_info[2], play_time, i))
 
     if len(musics) == 0:
         return "(None)"
-    if len(musics) == 1:
-        return musics[0][0]
-    musics = sorted(musics, key=lambda x: (-x[1], x[2]))
 
-    return musics[0][0]
+    return sorted(musics, key=lambda x: (-x[1], x[2]))[0][0]
 
 
 def get_play_time(start_time, end_time):
-    start_info = list(map(int, start_time.split(":")))
-    end_info = list(map(int, end_time.split(":")))
-
-    return (end_info[0] * 60 + end_info[1]) - (start_info[0] * 60 - start_info[1])
+    return (datetime.strptime(end_time, "%H:%M") - datetime.strptime(start_time, "%H:%M")).seconds // 60
 
 
 def convert_chords(chords: str):
@@ -57,13 +47,11 @@ m3 = "ABC"
 mi3 = ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]
 m4 = "A"
 mi4 = []
+m5 = "C#D"
+mi5 = ["12:00,12:02,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]
 
 print(solution(m1, mi1))
 print(solution(m2, mi2))
 print(solution(m3, mi3))
 print(solution(m4, mi4))
-
-# chords = music_info[3] * (play_time // len(music_info[3]) + 1)
-# chords = chords[:play_time]
-# if m in chords:
-#     musics.append((music_info[2], play_time, i))
+print(solution(m5, mi5))
