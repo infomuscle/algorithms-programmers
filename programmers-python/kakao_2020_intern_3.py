e476e053f0b399b5e4e9f997e0490aa4234ec02d
infomuscle <infomuscle10@gmail.com>
2021-01-01 02:44:26 +0900

    Kakao 2020 인턴 3 - 보석 쇼핑(백업)

--- programmers-python/kakao_2020_intern_3.py
+++ programmers-python/kakao_2020_intern_3.py
@@ -1,47 +1,33 @@
 def solution(gems):
-    gems_set = set(gems)
-    answers = []
+    diversity = len(set(gems))
+    left = 0
+    right = len(gems) - 1
 
-    p1, p2 = 0, 0
-    while p1 <= len(gems):
-        # print(p1, p2)
-        while gems_set != set(gems[p2:p1 + 1]) and p1 != len(gems):
-            p1 += 1
-        right = p1
+    while True:
+        if len(set(gems[left:right + 1])) == diversity:
+            right -= 1
+        else:
+            right += 1
+            break
 
-        lefts = []
-        while True:
-            if gems_set == set(gems[p2:right + 1]):
-                lefts.append(p2)
-            if p2 == right:
-                if len(lefts) != 0:
-                    p2 = p1
-                    p1 = lefts[-1]
-                break
-            p2 += 1
+    while True:
+        if len(set(gems[left:right + 1])) == diversity:
+            left += 1
+        else:
+            left -= 1
+            break
 
-        if len(lefts) != 0:
-            answers.append([lefts[-1] + 1, right + 1])
-        if p1 == p2:
-            p1 += 1
-            p2 += 1
-
-    min_range = min(a[1] - a[0] for a in answers)
-    min_answers = []
-    for a in answers:
-        if a[1] - a[0] == min_range:
-            min_answers.append(a)
-    answer = min_answers[0]
-
-    return answer
+    return [left + 1, right + 1]
 
 
 g1 = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
 g2 = ["AA", "AB", "AC", "AA", "AC"]
 g3 = ["XYZ", "XYZ", "XYZ"]
 g4 = ["ZZZ", "YYY", "NNNN", "YYY", "BBB"]
+g5 = ["T"]
 
 print(solution(g1))
 print(solution(g2))
 print(solution(g3))
 print(solution(g4))
+print(solution(g5))
