567669ee4e717cb5350150ce1c86388d9e36ffec
infomuscle <infomuscle10@gmail.com>
2021-01-01 02:15:31 +0900

    Kakao 2018-3-3 [3차] 파일명 정렬

--- programmers-python/kakao_2018_3_3.py
+++ programmers-python/kakao_2018_3_3.py
@@ -1,35 +1,28 @@
-def solution(msg):
-    answer = []
+def solution(files):
+    split = []
+    for file in files:
+        head, number, tail = "", "", ""
+        head_end = 0
+        for i in range(len(file) - 1):
+            if head == "" and not file[i].isdigit() and file[i + 1].isdigit():
+                head = file[:i + 1]
+                head_end = i
+            if number == "" and file[i].isdigit() and not file[i + 1].isdigit():
+                number = file[head_end + 1:i + 1]
+                tail = file[i + 1:]
+                break
+            if i == len(file) - 2:
+                number = file[head_end + 1:]
+        split.append([head, number, tail])
+    split = sorted(split, key=lambda x: (x[0].lower(), int(x[1])))
 
-    dictionary = {}
-    for i in range(26):
-        dictionary[chr(i + 65)] = i + 1
-
-    v = 27
-    ptr = 0
-    w_length = 1
-    while ptr < len(msg):
-        w = msg[ptr:ptr + w_length]
-        c = msg[ptr + w_length:ptr + w_length + 1]
-        if c == "":
-            answer.append(dictionary[w])
-            break
-        if w in dictionary and w + c not in dictionary:
-            answer.append(dictionary[w])
-            dictionary[w + c] = v
-            v += 1
-            ptr += w_length
-            w_length = 1
-            continue
-        w_length += 1
-
-    return answer
+    return ["".join(s) for s in split]
 
 
-m1 = "KAKAO"
-m2 = "TOBEORNOTTOBEORTOBEORNOT"
-m3 = "ABABABABABABABAB"
+f1 = ["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"]
+f2 = ["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]
+f3 = ["foo9.txt", "foo010bar020.zip", "F-15", "F2"]
 
-print(solution(m1))
-print(solution(m2))
-print(solution(m3))
+print(solution(f1))
+print(solution(f2))
+print(solution(f3))
