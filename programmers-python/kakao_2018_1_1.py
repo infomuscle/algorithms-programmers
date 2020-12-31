bfa849c74c9058d8e3ecd219a11605ba67dec116
infomuscle <infomuscle10@gmail.com>
2021-01-01 02:04:33 +0900

    Kakao 2018-1-1 [1차] 비밀지도

--- programmers-python/kakao_2018_1_1.py
+++ programmers-python/kakao_2018_1_1.py
@@ -1,50 +1,24 @@
-def solution(dartResult):
-    multiplier = ["S", "D", "T"]
-    option = ["*", "#"]
+def solution(n, arr1, arr2):
+    map1 = [str(bin(arr1[i]))[2:].zfill(n) for i in range(n)]
+    map2 = [str(bin(arr2[i]))[2:].zfill(n) for i in range(n)]
 
-    stack = []
+    treasure_map = []
+    for i in range(n):
+        line = ""
+        for j in range(n):
+            line += "#" if map1[i][j] == "1" or map2[i][j] == "1" else " "
+        treasure_map.append(line)
 
-    for i, d in enumerate(dartResult):
-        if d.isdigit():
-            if d == "1" and dartResult[i + 1] == "0":
-                stack.append(10)
-            elif d == "0" and dartResult[i - 1] == "1":
-                pass
-            else:
-                stack.append(int(d))
-        elif d in multiplier:
-            if d == "D":
-                squarer = 2
-            elif d == "T":
-                squarer = 3
-            else:
-                squarer = 1
-            stack.append(stack.pop() ** squarer)
-        elif d in option:
-            top = stack.pop()
-            if d == "*":
-                if len(stack) != 0:
-                    stack.append(stack.pop() * 2)
-                top *= 2
-            elif d == "#":
-                top = -top
-            stack.append(top)
-
-    return sum(stack)
+    return treasure_map
 
 
-d1 = "1S2D*3T"
-d2 = "1D2S#10S"
-d3 = "1D2S0T"
-d4 = "1S*2T*3S"
-d5 = "1D#2S*3S"
-d6 = "1T2D3D#"
-d7 = "1D2S3T*"
+n1 = 5
+a11 = [9, 20, 28, 18, 11]
+a21 = [30, 1, 21, 17, 28]
 
-print(solution(d1))
-print(solution(d2))
-print(solution(d3))
-print(solution(d4))
-print(solution(d5))
-print(solution(d6))
-print(solution(d7))
+n2 = 6
+a12 = [46, 33, 33, 22, 31, 50]
+a22 = [27, 56, 19, 14, 14, 10]
+
+print(solution(n1, a11, a21))
+print(solution(n2, a12, a22))
