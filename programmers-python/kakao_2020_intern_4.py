fff371b5a45a823a3f2ce9773cff500e91df1757
infomuscle <infomuscle10@gmail.com>
2021-01-08 02:03:42 +0900

    Kakao 2020 인턴 4 - 경주로 건설(백업)

--- programmers-python/kakao_2020_intern_4.py
+++ programmers-python/kakao_2020_intern_4.py
@@ -1,9 +1,27 @@
 def solution(board):
-    for row in board:
-        print(row)
-
     answer = 0
     n = len(board)
+    graph = get_graph(board, n)
+
+    stack = ["00"]
+    while stack:
+        cur = stack[-1]
+        if cur in graph and len(graph[cur]) != 0:
+            if graph[cur][-1] not in stack:
+                stack.append(graph[cur].pop())
+            else:
+                graph[cur].pop()
+                stack.pop()
+        if stack[-1] == str(n - 1) + str(n - 1):
+            break
+    print(stack)
+
+    straight, corner = 0, 0
+
+    return answer
+
+
+def get_graph(board, n):
     graph = {}
     for i in range(n):
         for j in range(n):
@@ -15,31 +33,7 @@ def solution(board):
                     if t[0] < n and t[0] >= 0 and t[1] < n and t[1] >= 0 and board[t[0]][t[1]] == 0:
                         graph[id].append(str(t[0]) + str(t[1]))
                 graph[id].sort()
-    print(graph)
-
-    road = []
-    stack = ["00"]
-    while stack:
-        cur = stack[-1]
-        if cur in graph and len(graph[cur]) != 0:
-            stack.append(graph[cur].pop())
-        else:
-            road.append(stack.pop())
-    road.reverse()
-    print(road)
-
-    idx = 0
-    for i, r in enumerate(road):
-        if r == str(n - 1) + str(n - 1):
-            idx = i
-            break
-    road = road[:idx + 1]
-    print(road)
-
-    straight, corner = 0, 0
-    pos = [0, 0]
-
-    return answer
+    return graph
 
 
 b1 = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -48,7 +42,7 @@ b2 = [[0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 
 b3 = [[0, 0, 1, 0], [0, 0, 0, 0], [0, 1, 0, 1], [1, 0, 0, 0]]
 b4 = [[0, 0, 0, 0, 0, 0], [0, 1, 1, 1, 1, 0], [0, 0, 1, 0, 0, 0], [1, 0, 0, 1, 0, 1], [0, 1, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0]]
 
-# print(solution(b1))
+print(solution(b1))
 print(solution(b2))
-# print(solution(b3))
-# print(solution(b4))
+print(solution(b3))
+print(solution(b4))
