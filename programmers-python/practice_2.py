aafe4d3929cf9bc0ca8ed9bd421afaa064d9b660
infomuscle <infomuscle10@gmail.com>
2021-01-10 18:08:23 +0900

    연습문제 2 - 같은 숫자는 싫어

--- programmers-python/practice_2.py
+++ programmers-python/practice_2.py
@@ -1,9 +1,5 @@
 def solution(arr):
-    answer = []
-
-    answer.append(arr[0])
-    for i in range(1, len(arr)):
-        if arr[i] != arr[i-1]:
-            answer.append(arr[i])
+    answer = [arr[0]]
+    answer.extend([arr[i] for i in range(1, len(arr)) if arr[i] != arr[i - 1]])
 
     return answer
