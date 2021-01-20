9da488669a846bfab367e34ac96d2f2aff3f547a
infomuscle <infomuscle10@gmail.com>
2021-01-20 21:21:54 +0900

    연습문제 12 - 나누어 떨어지는 숫자 배열

--- programmers-python/practice_12.py
+++ programmers-python/practice_12.py
@@ -2,7 +2,7 @@ def solution(arr, divisor):
     answer = []
 
     for a in arr:
-        if a%divisor == 0:
+        if a % divisor == 0:
             answer.append(a)
 
     if len(answer) == 0:
@@ -10,11 +10,12 @@ def solution(arr, divisor):
 
     return sorted(answer)
 
-arr1 = [5,9,7,10]
-d1 =5
-arr2 = [2,36,1,3]
+
+arr1 = [5, 9, 7, 10]
+d1 = 5
+arr2 = [2, 36, 1, 3]
 d2 = 1
-arr3 = [3,2,6]
+arr3 = [3, 2, 6]
 d3 = 10
 
 print(solution(arr1, d1))
