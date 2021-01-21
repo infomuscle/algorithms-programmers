2bfe71ee2e3d8a41981d7cab7ee905143f678fc8
infomuscle <infomuscle10@gmail.com>
2021-01-21 14:49:21 +0900

    연습문제 4 - 두 정수 사이의 합

--- programmers-python/practice_4.py
+++ programmers-python/practice_4.py
@@ -2,21 +2,22 @@ def solution(a, b):
     answer = 0
 
     if b < a:
-       s, e = b, a
+        s, e = b, a
     else:
         s, e = a, b
 
-    for i in range((e-s+1)//2):
+    for i in range((e - s + 1) // 2):
         answer += (s + e)
 
-    if (e-s+1) % 2 == 1:
+    if (e - s + 1) % 2 == 1:
         answer += (s + e) // 2
 
     return answer
 
-a1, b1 = 3,5
-a2, b2 = 3,3
-a3, b3 = 5,3
+
+a1, b1 = 3, 5
+a2, b2 = 3, 3
+a3, b3 = 5, 3
 
 print(solution(a1, b1))
 print(solution(a2, b2))
