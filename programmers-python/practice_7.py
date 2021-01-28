bde8ac9a96b126daf4f3c1662505c303a24855bf
infomuscle <infomuscle10@gmail.com>
2021-01-28 15:43:18 +0900

    연습문제 7 - 서울에서 김서방 찾기

--- programmers-python/practice_7.py
+++ programmers-python/practice_7.py
@@ -1,7 +1,5 @@
 def solution(seoul):
-    for i in range(len(seoul)):
-        if seoul[i] == "Kim":
-            return "김서방은 " + str(i) + "에 있다"
+    return "김서방은 %d에 있다" % seoul.index("Kim")
 
 
 s1 = ["Jane", "Kim"]
